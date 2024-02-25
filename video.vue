476e21df7bc55d911be15b96b077133365e1b4fc
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-02-25 12:05:38 +0100

    Update video.vue

--- video.vue
+++ video.vue
@@ -4,3 +4,16 @@
     <VideoStream :src="http://#/example" />
   </div>
 </template>
+
+<script>
+  import VideoStream from './VideoStream.vue'; 
+
+  export default { 
+    name: 'App', 
+    components: {
+      VideoStream
+    },
+    data() {
+
+    }
+  }
